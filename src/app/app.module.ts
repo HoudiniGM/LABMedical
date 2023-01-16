c74ffc337c7c6048844d77c8ac4e8551778d467e
João Vitor Anacleto <joaovittormtm@gmail.com>
2023-01-16 17:42:22 -0300

    Criação de componentes da tela de login
    Componentes: tela de login, modal de cadastro e modal de alerta.
    Validações ainda pendentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
+import { ModalSignupComponent } from './components/modal-signup/modal-signup.component';
+import { ModalForgotPasswordComponent } from './components/modal-forgot-password/modal-forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    ModalSignupComponent,
+    ModalForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/modal-forgot-password/modal-forgot-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-forgot-password',
+  templateUrl: './modal-forgot-password.component.html',
+  styleUrls: ['./modal-forgot-password.component.css']
+})
+export class ModalForgotPasswordComponent {
+
+}
