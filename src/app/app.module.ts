72dbc3bbc67d50a61c051cfb450bb2d5023ed2f0
João Vitor Anacleto <joaovittormtm@gmail.com>
2023-01-17 18:40:06 -0300

    :sparkles: : toolbar e menu lateral
    Criação do menu lateral e da toolbar, componentes que fazem parte de toda a aplicação, menos da tela de login.
    Sem funções ainda.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
 import { ModalSignupComponent } from './components/modal-signup/modal-signup.component';
 import { ModalForgotPasswordComponent } from './components/modal-forgot-password/modal-forgot-password.component';
+import { ApplicationScreenComponent } from './pages/application-screen/application-screen.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginScreenComponent,
     ModalSignupComponent,
-    ModalForgotPasswordComponent
+    ModalForgotPasswordComponent,
+    ApplicationScreenComponent,
+    ToolbarComponent,
+    AsideMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/application-screen/application-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-screen',
+  templateUrl: './application-screen.component.html',
+  styleUrls: ['./application-screen.component.css']
+})
+export class ApplicationScreenComponent {
+
+}
